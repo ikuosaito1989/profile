793e203d17eaf0bea7d3bf7826a2de076a12a8ae
ikuo saito <ikuosaito1989@gmail.com>
2019-12-26 14:42:06 +0900

    portfolios storeを作成

---
+++ profile/store/portfolios.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  portfolios: {}
+})
+
+export const actions = {
+  async getPortfolios(context) {
+    const portfolios = await this.$axios.$get('profile/portfolios')
+    context.commit('setPortfolios', { portfolios })
+  }
+}
+
+export const mutations = {
+  setPortfolios(state, { portfolios }) {
+    state.portfolios = portfolios
+  }
+}
+
+export const getters = {
+  portfolios(state) {
+    return state.portfolios
+  }
+}
