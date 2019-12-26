0373f924641d10fca588b68698d1b44d8e2c3b19
ikuo saito <ikuosaito1989@gmail.com>
2019-12-26 18:10:15 +0900

    テストできる状態にする

---
+++ profile/test/PortfolioItem.spec.js
@@ -0,0 +1,10 @@
+import '@/test/spec_helper.js'
+import { mount } from '@vue/test-utils'
+import PortfolioItem from '@/components/PortfolioItem.vue'
+
+describe('PortfolioItem', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(PortfolioItem)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
