2eec24f1537e89df4a788777f291c7f8a25ec811
ikuo saito <ikuosaito1989@gmail.com>
2019-12-31 17:51:10 +0900

    skills storeを作成

---
+++ src/store/skills.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  skills: {}
+})
+
+export const actions = {
+  async showSkills(context) {
+    const skills = await this.$axios.$get('/api/profile/skills')
+    context.commit('setSkills', { skills })
+  }
+}
+
+export const mutations = {
+  setSkills(state, { skills }) {
+    state.skills = skills
+  }
+}
+
+export const getters = {
+  skills(state) {
+    return state.skills
+  }
+}
