59991be01749cb1f273259fb2e17f2b53e12a6e8
ikuo saito <ikuosaito1989@gmail.com>
2020-01-07 23:27:00 +0900

    ポートフォリオ詳細ページを作成

--- src/pages/portfolio/_id.vue
+++ src/pages/portfolio/_id.vue
@@ -1,39 +1,53 @@
 <template>
-  <svg
-    class="NuxtLogo"
-    width="245"
-    height="180"
-    viewBox="0 0 452 342"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g fill="none" fill-rule="evenodd">
-      <path
-        d="M139 330l-1-2c-2-4-2-8-1-13H29L189 31l67 121 22-16-67-121c-1-2-9-14-22-14-6 0-15 2-22 15L5 303c-1 3-8 16-2 27 4 6 10 12 24 12h136c-14 0-21-6-24-12z"
-        fill="#00C58E"
-      />
-      <path
-        d="M447 304L317 70c-2-2-9-15-22-15-6 0-15 3-22 15l-17 28v54l39-67 129 230h-49a23 23 0 0 1-2 14l-1 1c-6 11-21 12-23 12h76c3 0 17-1 24-12 3-5 5-14-2-26z"
-        fill="#108775"
-      />
-      <path
-        d="M376 330v-1l1-2c1-4 2-8 1-12l-4-12-102-178-15-27h-1l-15 27-102 178-4 12a24 24 0 0 0 2 15c4 6 10 12 24 12h190c3 0 18-1 25-12zM256 152l93 163H163l93-163z"
-        fill="#2F495E"
-        fill-rule="nonzero"
-      />
-    </g>
-  </svg>
+  <div class="modal-body text-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <!-- Portfolio Modal - Title -->
+          <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">
+            {{ selectedPortfolio.name }}
+          </h2>
+          <!-- Icon Divider -->
+          <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon">
+              <i class="fas fa-star"></i>
+            </div>
+            <div class="divider-custom-line"></div>
+          </div>
+          <!-- Portfolio Modal - Image -->
+          <img
+            :src="selectedPortfolio.thumbnailUrl"
+            class="img-fluid rounded mb-5"
+            alt=""
+          />
+          <!-- Portfolio Modal - Text -->
+          <p class="mb-5">
+            {{ selectedPortfolio.description }}
+          </p>
+          <n-link to="/">
+            <button class="btn btn-primary" href="#" data-dismiss="modal">
+              <i class="fas fa-times fa-fw"></i>
+              もどる
+            </button>
+          </n-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-<style>
-.NuxtLogo {
-  animation: 1s appear;
-}
 
-@keyframes appear {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
+<script>
+export default {
+  computed: {
+    selectedPortfolio() {
+      return this.$store.getters['portfolios/selectedPortfolio']
+    }
+  },
+  async fetch(context) {
+    await context.store.dispatch('portfolios/selectPortfolio', {
+      portfolioId: context.params.id
+    })
   }
 }
-</style>
+</script>
--- test/pages/portfolio/_id.spec.js
+++ test/pages/portfolio/_id.spec.js
@@ -1,10 +1,33 @@
 import '@/test/spec_helper.js'
 import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
 import _id from '@/src/pages/portfolio/_id.vue'
 
 describe('_id', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(_id)
+  const store = new Vuex.Store({
+    getters: {
+      'portfolios/selectedPortfolio': () =>
+        require('@/test/fixtures/portfolios.json')[0]
+    },
+    actions: {
+      'portfolios/selectPortfolio': jest.fn()
+    }
+  })
+  store.dispatch = jest.fn()
+  context = {
+    store,
+    params: { id: 1 }
+  }
+  const wrapper = mount(_id, { store })
+
+  test('Vueのインスタンスを返すこと', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('selectPortfolioが呼ばれること', () => {
+    _id.fetch(context)
+    expect(
+      context.store.dispatch
+    ).toHaveBeenCalledWith('portfolios/selectPortfolio', { portfolioId: 1 })
   })
 })
