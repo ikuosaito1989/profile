c98403199cd3626e29fae3ed8d5b8a844236bbde
ikuo saito <ikuosaito1989@gmail.com>
2020-01-09 23:01:35 +0900

    transitionを追加

--- src/components/portfolio.vue
+++ src/components/portfolio.vue
@@ -2,16 +2,18 @@
   <div class="col-md-6 col-lg-4">
     <h5 style="text-align: center;">{{ name }}</h5>
     <div class="portfolio-item mx-auto">
-      <nuxt-link v-bind:to="{ name: 'portfolio-id', params: { id: id } }">
-        <div
-          class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"
-        >
-          <div class="portfolio-item-caption-content text-center text-white">
-            <i class="fas fa-plus fa-3x"></i>
+      <transition>
+        <nuxt-link v-bind:to="{ name: 'portfolio-id', params: { id: id } }">
+          <div
+            class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100"
+          >
+            <div class="portfolio-item-caption-content text-center text-white">
+              <i class="fas fa-plus fa-3x"></i>
+            </div>
           </div>
-        </div>
-        <img :src="thumbnailUrl" class="img-fluid" alt="" />
-      </nuxt-link>
+          <img :src="thumbnailUrl" class="img-fluid" alt="" />
+        </nuxt-link>
+      </transition>
     </div>
   </div>
 </template>
